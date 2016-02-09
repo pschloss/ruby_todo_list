72cfd6717b27bf1327a70241af89516cf86bee53
Pat Schloss <pschloss@umich.edu>
2016-02-09 07:34:00 -0500

    Through page 4

---
+++ todo.rb
@@ -0,0 +1,39 @@
+# Classes
+class List
+	def initialize
+	end
+
+	def add_task
+	end
+
+	def show_list
+	end
+
+	def read_tast_from_file
+	end
+
+	def write_list_to_file
+	end
+
+	def delete_task
+	end
+
+	def update_task
+	end
+
+end
+
+
+class Task
+	def initialize
+	end
+
+end
+
+
+
+#actions
+if __FILE__ == $PROGRAM_NAME
+	my_list = List.new
+	puts 'You have created a new list'
+end
